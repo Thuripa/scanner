2cc461420d22937ed7e6181c0a5845a0cb0f98af
Thuripa <tainhabichoesperto@gmail.com>
2023-09-29 06:16:56 -0300

    Foi implementado um esquema multithreading e foi incrementado o número de palavras na wordlist

--- scan_dns.py
+++ scan_dns.py
@@ -1,17 +1,27 @@
 # Guilherme Melo de Jesus - Baseado no livro 'Python para Pentest' de Daniel Moreno
 # Ferramenta que automatiza enumeração de servidores DNS
 
+# Importa biblioteca socket
 import socket
+import threading
 
+
+def thread():
+    # Para cada nome
+    for nome in nomes:
+        dns = nome.strip() + "." + dominio
+
+        try:
+            print(dns + ": " + socket.gethostbyname(dns))
+        except socket.gaierror:
+            pass
+
+
+# Domínio a ser testado
 dominio = "google.com"
 
+# Abre o arquivo wordlist
 with open("brute-force.txt") as arquivo:
     nomes = arquivo.readlines()
 
-for nome in nomes:
-    dns = nome.strip() + "." + dominio
-
-    try:
-        print(dns + ": " + socket.gethostbyname(dns))
-    except socket.gaierror:
-        pass
+threading.Thread(target=thread).start()
