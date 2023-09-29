118d56ab411626171ade95a12e4cb84d3d4c70fa
Thuripa <tainhabichoesperto@gmail.com>
2023-09-29 05:27:26 -0300

    A ferramenta agora realiza a busca dos nomes em um arquivo de texto (brute-force.txt)

--- scan_dns.py
+++ scan_dns.py
@@ -1,4 +1,17 @@
 # Guilherme Melo de Jesus - Baseado no livro 'Python para Pentest' de Daniel Moreno
 # Ferramenta que automatiza enumeração de servidores DNS
 
-print ("Hello World!")
+import socket
+
+dominio = "google.com"
+
+with open("brute-force.txt") as arquivo:
+    nomes = arquivo.readlines()
+
+for nome in nomes:
+    dns = nome.strip() + "." + dominio
+
+    try:
+        print(dns + ": " + socket.gethostbyname(dns))
+    except socket.gaierror:
+        pass
